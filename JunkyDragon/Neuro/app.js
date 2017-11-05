38d291395d73e5519e84d6c110017978ffe2d5cf
JunkyDragon <junkydragon01@gmail.com>
2017-11-06 08:22:48 +0900

    error

--- JunkyDragon/Neuro/app.js
+++ JunkyDragon/Neuro/app.js
@@ -1,32 +1,47 @@
-var sleep = require('sleep');
-var io = require('socket.io-client');
-var socket = io.connect("http://163.44.165.68:8080");
-var Cylon = require("cylon");
-"use strict";
+try {
+  var sleep = require('sleep');
+  var io = require('socket.io-client');
+  var socket = io.connect("http://163.44.165.68:8080");
+  var Cylon = require("cylon");
+  var wave = null
+  var d = null "use strict";
 
-function send(string, data) {
-  socket.emit(string, data);
-}
-Cylon.robot({
-  connections: {
-    neurosky: {
-      adaptor: "neurosky",
-      port: "COM3"
-    }
-  },
-
-  devices: {
-    headset: {
-      driver: "neurosky"
-    }
-  },
-
-  work: function(my) {
-    my.headset.on("eeg", function(data) {
-      send("eeg", data);
-      console.log("Data:", data);
-    });
+  function send(string, data) {
+    socket.emit(string, data);
   }
-});
+  Cylon.robot({
+    connections: {
+      neurosky: {
+        adaptor: "neurosky",
+        port: "COM3"
+      }
+    },
 
-Cylon.start();
+    devices: {
+      headset: {
+        driver: "neurosky"
+      }
+    },
+
+    work: function(my) {
+      my.headset.on("attention", function(data) {
+        if (data < 100 && data != 0) {
+          console.log("attention:", data);
+          socket.emit("attention", data);
+          socket.on('attention', function(data) {
+            if (data != 'success') {
+              throw new Exception("errno", "Network Error");
+            }
+          });
+          sleep.msleep(1300);
+        } else {
+          console.log("trash");
+        }
+      });
+    }
+  });
+
+  Cylon.start();
+} catch(e) {
+  
+}
--- JunkyDragon/Neuro/send.js
+++
@@ -1,7 +0,0 @@
-var io = require('socket.io-client');
-var socket = io.connect("http://163.44.165.68:8080");
-
-socket.emit('attention', "hello");
-socket.on('attention', function(data){
-  console.log(data);
-});
